73a096466364b754a3ec4c5f81862290f8a4cfba
Tianyi (Bruce) Chen <152252677+TyBruceChen@users.noreply.github.com>
2024-01-18 18:45:17 -0700

    Update grad_cam.py

--- grad_cam_code/grad_cam.py
+++ grad_cam_code/grad_cam.py
@@ -6,13 +6,15 @@ from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import math
 
 
 class GradCAM:
   def __init__(self,model,
                img_path:str,
                layer_idx: int = 2,
-               input_shape = (224,224)
+               input_shape = (224,224),
+               model_type: str = 'Normal'
                ):
     """
     params:
@@ -21,13 +23,17 @@ class GradCAM:
     input_shape: the image shape to put into the model
     model: the model you want to visualize
     img_path: the path of the tested image
+    model_type: some special model need addtional method to process the activations
+      in order to get Grad-CAM. Currently, there's only function to handel vision
+      transformer.
     """
     self.model = model
     self.layer_idx = -layer_idx
     self.img_path = img_path
     self.input_shape = input_shape
+    self.model_type = model_type
+    print('The model types you can select from are \'Normal\', \'ViT\'')
 
-  
   def __call__(self):
     model = self.model
 
@@ -52,18 +58,29 @@ class GradCAM:
     print(f'Activation Shape:{activations.shape}')
 
     prediction_logits = classifier(activations) #the activation is fed into rest layers to get the prediction tensor
+    print(f'Prediction_logits Shape:{prediction_logits.shape}')
+    print(class_Idx)
 
-    prediction_logits = prediction_logits[:,class_Idx]  #only use the specific class to back propagate
+    if self.model_type == 'Normal':
+      prediction_logits = prediction_logits[:,class_Idx]  #only use the specific class to back propagate
+    elif self.model_type == 'ViT':
+      prediction_logits = prediction_logits[:,:,class_Idx]
 
     grad_output = torch.ones_like(prediction_logits)
     print(grad_output.shape)
+
     prediction_logits.backward(gradient = grad_output)  #according to pytorch, backward() should specify
                                                           #with a tensor which its length is the same as backward tensor
                                                             #when the tensor contains more than one number
     d_act = activations.grad  #get the gradient of activation from target layer w.r.t. the specified category
 
-    d_act = d_act.permute(0,2,3,1)  #(1,C,H,W) -> (1,H,W,C)
-    activations = activations.permute(0,2,3,1)
+    if self.model_type == 'Normal':
+      d_act = d_act.permute(0,2,3,1)  #(1,C,H,W) -> (1,H,W,C)
+      activations = activations.permute(0,2,3,1)
+    elif self.model_type == 'ViT':
+      d_act = self.output_decompose_vit_grad_cam(d_act)
+      activations = self.output_decompose_vit_grad_cam(activations[:,:,:])
+
     pooled_grads = torch.mean(d_act,dim = (0,1,2))  #according to the paper, the pooling happens all axis except the channel dim
 
     heatmap = activations.detach().numpy()[0] #for tensors where its requires_grad = True, need detach() function to convert to ndarray
@@ -73,16 +90,16 @@ class GradCAM:
     for  i in range(d_act.shape[-1]):
       heatmap[:,:,i] *= pooled_grads[i]
     heatmap = np.mean(heatmap, axis = -1)
-    self.heatmap = np.uint8(255*np.maximum(heatmap,0)/np.max(heatmap))  #keep the logits that are greater than zero 
+    self.heatmap = np.uint8(255*np.maximum(heatmap,0)/np.max(heatmap))  #keep the logits that are greater than zero
       #in the paper, that is to say, only keep the positive influence with the specific class.
         #then normalize the heatmap and recale its value range from 0 to 255.
 
-  
+
   def origin_cam_visualization(self):
     plt.matshow(self.heatmap)
     plt.show
 
-  
+
   def imposing_visualization(self):
     alpha = 0.6 #how much CAM will overlap on original image
     jet = cm.get_cmap('jet')
@@ -101,3 +118,15 @@ class GradCAM:
     plt.imshow(img_cam/255) #print the overlapped image (origin + cam)
     plt.subplot(1,2,2)
     plt.imshow(jet_heatmap/255) #print the cam
+
+
+  def output_decompose_vit_grad_cam(self, vit_input):
+    #decompose on vit's sequence dimension
+    _, HW, C = vit_input.shape
+    print(HW)
+    HW = int(math.sqrt(HW))
+    print(HW)
+    vit_output = torch.reshape(vit_input,(1,HW,HW,C))
+
+    #vit_output = vit_output.permute(0,3,1,2)
+    return vit_output
